9a2d8b83f8d39c46fc8cf15a308eece7b17b3c53
Elena Itin <elenakochegina@gmail.com>
2022-06-30 15:11:18 +0300

    Работа с DOM. Загрузка документа

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,124 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  static subElement = null;
 
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = [...headerConfig];
+    this.data = [...data];
+
+    this.sort();
+    this.render();
+
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.getTemplate();
+    this.element = wrapper.firstElementChild;
+    setTimeout(() => {
+      SortableTable.subElement = this.getSubElement();
+    }, 1);
+  }
+
+  getTemplate() {
+    return `
+    <div data-element="productsContainer" class="products-list__container">
+    <div class="sortable-table">
+    <div data-element="header" class="sortable-table__header sortable-table__row">
+    ${this.getHeader()}
+    </div>
+    <div data-element="body" class="sortable-table__body">
+    ${this.getBody()}
+    </div>
+    <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+
+    <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+      <div>
+        <p>No products satisfies your filter criteria</p>
+        <button type="button" class="button-primary-outline">Reset all filters</button>
+      </div>
+    </div>
+
+  </div>
+</div>`;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
+
+  getHeader() {
+    if (this.headerConfig.length !== 0) {
+      return this.headerConfig.map(item => {
+        return `<div className="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}" data-order="asc">
+          <span>${item.title}</span>
+        </div>`;
+      }).join("");
+    }
+
+  }
+
+  getBody() {
+    if (this.data !== 0) {
+      return this.data.map(dataItem => {
+        return `
+        <a href="/products/${dataItem.id}" class="sortable-table__row">
+        ${this.getDataItems(dataItem)}
+        </a>`;
+      }).join("");
+    }
+  }
+
+  getDataItems(dataItem) {
+    return this.headerConfig.map(headerItem => {
+      return headerItem.id !== 'images' ? `<div class= "sortable-table__cell" >${dataItem[headerItem.id]} </div>` : this.getImage(headerItem.template, dataItem[headerItem.id]);
+    }).join("");
+  }
+
+
+  getImage(template, arr = []) {
+    if (template) {
+      return template(arr);
+    } else {
+      return `
+    <div class="sortable-table__cell">
+      <img class="sortable-table-image" alt="Image" src="http://magazilla.ru/jpg_zoom1/246743.jpg">
+    </div>`;
+    }
+  }
+
+
+  sort(fieldValue = 'title', orderValue = 'asc') {
+    const res = [...this.data];
+    const sortType = this.headerConfig.filter(item => (item.id === fieldValue))[0].sortType;
+    if (sortType === 'string') {
+      if (orderValue === 'desc') {
+        res.sort((a, b) => b[fieldValue].localeCompare(a[fieldValue], ['ru-RU', 'en-US'], {caseFirst: 'upper'}));
+      } else if (orderValue === 'asc') {
+        res.sort((a, b) => a[fieldValue].localeCompare(b[fieldValue], ['ru-RU', 'en-US'], {caseFirst: 'upper'}));
+      }
+    } else if (sortType === 'number') {
+      if (orderValue === 'desc') {
+        res.sort((a, b) => b[fieldValue] - a[fieldValue]);
+      } else if (orderValue === 'asc') {
+        res.sort((a, b) => a[fieldValue] - b[fieldValue]);
+      }
+    }
+    this.data = res;
+    if (SortableTable.subElement) {
+      SortableTable.subElement.innerHTML = this.getBody();
+    }
+  }
+
+
+  getSubElement() {
+    return document.querySelector(".sortable-table__body");
   }
 }
 
